098b5e27c64c71088abb5ae96092559da8d6658e
Josh <Josh@cannacoin.cc>
2014-05-05 01:02:20 -0700

    First Commit
    Node.js IRC Tipbot for Cannacoin

---
+++ README.md
@@ -0,0 +1,95 @@
+node-tip-bot is an open-source node.js IRC bot for tipping with altcoins. It uses [node-cannacoin](https://github.com/cannacoin-project/node-cannacoin) for integration with Litecoin's JSON RPC API.
+
+# Instalation
+To install node-tip-bot simply clone this repo and install dependencies:
+```bash
+git clone https://github.com/unek/node-tip-bot
+cd node-tip-bot
+npm install
+```
+After installation proceed to the configuration.
+
+# Configuration
+To configure, copy the `config/config.sample.yml` file to `config/config.yml`.
+
+## connection
+IRC network connection info.
+* **host** - hostname of the IRC server
+* **port** - port of the IRC server
+* **secure** - use secured connection
+* **status_command** - NickServ command to get nick's login status, ACC on freenode, STATUS on some other networks
+
+## login
+IRC network connection and login info.
+* **nickname** - bot's nickname
+* **username** - bot's username
+* **realname** - bot's realname
+* **nickserv_password** - nickserv password to identify with
+
+## channels
+List of channels to auto-join to.
+
+## webadmin
+Web interface settings.
+* **enabled** - enabled web admin
+* **port** - port to bound to
+* **users** - list of users with access to web interface in `name: password` format
+
+## log
+Logging settings.
+* **file** - file to log to. Set to `false` to disable logging to file.
+
+## rpc
+JSON RPC API connection info.
+* **host** - JSON RPC API hostname
+* **port** - API port (by default 22555 for cannacoin)
+* **user** - API username
+* **pass** - API password (keep that secure)
+
+## coin
+Basic coin settings.
+* **withdrawal_fee** - fee charged on withdraw to cover up txfee, the rest goes to bot's wallet.
+* **min_withdraw** - minimum amount of coins to withdraw
+* **min_confirmations** - minimum amount of confirmations needed to tip/withdraw coins
+* **min_tip** - minimum amount of coins to tip
+* **short_name** - short coin's name (eg. `Đ` or `DOGE`)
+* **full_name** - full coin's name (eg. `cannacoin`)
+
+## commands
+Here you can restrict some commands to work only on PM/channel.
+
+## messages
+Whatever the bot says. Supports expandable variables (eg. `%nick%` for bot's nick). By default all config vars from `rpc` section are available.
+
+# How does it work?
+Every nickname has it's own account in your wallet. When tipping or withdrawing, bot checks if user is registered and identified with NickServ. If so, he moves the money from one account to another, or when withdrawing, transfers coins to other wallet.
+
+# How to run it?
+Before running the bot, you have to be running your coin daemon with JSON-RPC API enabled. To enable, add this to your coin daemon configuration file (eg. `~/.cannacoin/cannacoin.conf`):
+```ini
+server=1
+daemon=1
+rpcuser=<your username>
+rpcpassword=<your super secret password>
+rpcallowip=<your bot's ip address or just 127.0.0.1 if hosted on the same machine>
+```
+
+To run the bot simply use `node bin/tipbot` or `npm start`.
+
+## Commands
+
+| **Command** | **Arguments**     | **Description**
+|-------------|-------------------|--------------------------------------------------------------------
+| `balance`   |                   | displays your current wallet balance
+| `address`   |                   | displays address where you can send your funds to the tip bot
+| `withdraw`  | `<address>`       | withdraws your whole wallet balance to specified address
+| `tip`       | `<nick> <amount>` | sends the specified amount of coins to the specified nickname
+| `help`      |                   | displays configured help message (by default similiar to this one)
+| `terms`     |                   | displays terms and conditions for using the tip bot
+
+## Donations
+Donations are welcome! If you like my bot and want to donate me, here are my wallet addresses:
+
+* **Dogecoin**: DGLT1pTSKAXLSSSz1NGFgZNLioDyHdaDyA
+* **Flappycoin**: FPEBPCC1sr9FAJ4DbD3r4YFwMqrr9TppeK
+* **Mintcoin**: MmfHmeWGUbzzd4he4u9yXRu5tQGLs1QNDL
---
+++ bin/tipbot.js
@@ -0,0 +1,371 @@
+var irc    = require('irc')
+, winston  = require('winston')
+, fs       = require('fs')
+, yaml     = require('js-yaml')
+, coin     = require('node-cannacoin')
+, webadmin = require('../lib/webadmin/app');
+
+// check if the config file exists
+if(!fs.existsSync('./config/config.yml')) {
+  winston.error('Configuration file doesn\'t exist! Please read the README.md file first.');
+  process.exit(1);
+}
+
+// handle sigint
+process.on('exit', function() {
+  winston.info('Exiting...');
+  if(client != null) {
+    client.disconnect('My master ordered me to leave.');
+  }
+});
+
+// load settings
+var settings = yaml.load(fs.readFileSync('./config/config.yml', 'utf-8'));
+
+// load winston's cli defaults
+winston.cli();
+
+// write logs to file
+if(settings.log.file) {
+  winston.add(winston.transports.File, {
+    filename: settings.log.file
+  , level: 'info'});
+}
+
+// connect to coin json-rpc
+winston.info('Connecting to coind...');
+
+var coin = coin({
+  host: settings.rpc.host
+, port: settings.rpc.port
+, user: settings.rpc.user
+, pass: settings.rpc.pass
+});
+
+coin.getBalance(function(err, balance) {
+  if(err) {
+    winston.error('Could not connect to %s RPC API! ', settings.coin.full_name, err);
+    process.exit(1);
+    return;
+  }
+
+  var balance = typeof(balance) == 'object' ? balance.result : balance;
+  winston.info('Connected to JSON RPC API. Current total balance is %d' + settings.coin.short_name, balance);
+})
+
+// run webadmin
+if(settings.webadmin.enabled)
+{
+  winston.info('Running webadmin on port %d', settings.webadmin.port);
+  webadmin.app(settings.webadmin.port, coin, settings, winston);
+}
+
+// connect to the server
+winston.info('Connecting to the server...');
+
+var client = new irc.Client(settings.connection.host, settings.login.nickname, {
+  port:   settings.connection.port
+, secure: settings.connection.secure
+
+, channels: settings.channels
+, userName: settings.login.username
+, realName: settings.login.realname
+
+, debug: settings.connection.debug
+});
+
+// gets user's login status
+irc.Client.prototype.getLoginStatus = function(nickname, callback) {
+  // request login status
+  this.say('NickServ', settings.connection.status_command + ' ' + nickname);
+
+  // wait for response
+  var listener = function(from, to, message) {
+   // proceed only on NickServ's ACC response
+    var regexp = new RegExp('^(\\S+) ' + settings.connection.status_command + ' (\\d)');
+    if(from != undefined && from.toLowerCase() == 'nickserv' && regexp.test(message)) {
+      var match = message.match(regexp);
+      var user  = match[1];
+      var level = match[2];
+
+      // if the right response, call the callback and remove this listener
+      if(user.toLowerCase() == nickname.toLowerCase()) {
+        callback(Number(level));
+        this.removeListener('notice', listener);
+      }
+    }
+  }
+
+  this.addListener('notice', listener);
+}
+
+// gets a empty coin address
+irc.Client.prototype.getAddress = function(nickname, callback) {
+  winston.debug('Requesting address for %s', nickname);
+  coin.send('getaccountaddress', nickname.toLowerCase(), function(err, address) {
+    if(err) {
+      winston.error('Something went wrong while getting address. ' + err);
+      callback(err);
+
+      return false;
+    }
+
+    callback(false, address);
+  });
+}
+
+String.prototype.expand = function(values) {
+  var global = {
+    nick: client.nick
+  }
+  return this.replace(/%([a-zA-Z_]+)%/g, function(str, variable) {
+    return typeof(values[variable]) == 'undefined' ? 
+      (typeof(settings.coin[variable]) == 'undefined' ? 
+        (typeof(global[variable]) == 'undefined' ?
+          str : global[variable]) : settings.coin[variable]) : values[variable];
+  });
+}
+
+// basic handlers
+client.addListener('registered', function(message) {
+  winston.info('Connected to %s.', message.server);
+
+  client.say('NickServ', 'IDENTIFY ' + settings.login.nickserv_password);
+});
+
+client.addListener('error', function(message) {
+  winston.error('Received an error from IRC network: ', message);
+});
+
+client.addListener('message', function(from, channel, message) {
+  var match = message.match(/^(!?)(\S+)/);
+  if(match == null) return;
+  var prefix  = match[1];
+  var command = match[2];
+
+  if(settings.commands[command]) {
+    if(channel == client.nick && settings.commands[command].pm === false) return;
+    if(channel != client.nick && (settings.commands[command].channel === false || prefix != '!')) return;
+  } else {
+    return;
+  }
+
+  // if pms, make sure to respond to pms instead to itself
+  if(channel == client.nick) channel = from;
+
+  // comands that don't require identifying
+  if(command == 'help' || command == 'terms')
+  {
+    for(var i = 0; i < settings.messages[command].length; i++) {
+      var message = settings.messages[command][i];
+      client.say(channel, message.expand({}));
+    }
+
+    return;
+  }
+
+  // if not that, message will be undefined for some reason
+  // todo: find a fix for that
+  var msg = message;
+  client.getLoginStatus(from, function(status) {
+    var message = msg;
+    // check if the sending user is logged in (identified) with nickserv
+    if(status != 3) {
+      winston.info('%s tried to use command `%s`, but is not identified.', from, message);
+      client.say(channel, settings.messages.not_identified.expand({name: from}));
+      return;
+    }
+
+    switch(command) {
+      case 'tip':
+        var match = message.match(/^.?tip (\S+) ([\d\.]+)/);
+        if(match == null || match < 3) {
+          client.say(channel, 'Usage: !tip <nickname> <amount>')
+          return;
+        }
+        var to     = match[1];
+        var amount = Number(match[2]);
+
+        if(isNaN(amount)) {
+          client.say(channel, settings.messages.invalid_amount.expand({name: from, amount: match[2]}));
+          return;
+        }
+
+        if(to.toLowerCase() == from.toLowerCase()) {
+          client.say(channel, settings.messages.tip_self.expand({name: from}));
+          return;
+        }
+
+        if(amount < settings.coin.min_tip) {
+          client.say(channel, settings.messages.tip_too_small.expand({from: from, to: to, amount: amount}));
+          return;
+        }
+        // check balance with min. 5 confirmations
+        coin.getBalance(from.toLowerCase(), settings.coin.min_confirmations, function(err, balance) {
+          if(err) {
+            winston.error('Error in !tip command.', err);
+            client.say(channel, settings.messages.error.expand({name: from}));
+            return;
+          }
+          var balance = typeof(balance) == 'object' ? balance.result : balance;
+
+          if(balance >= amount) {
+            client.getAddress(to, function(err, to_address) { // get the address to actually create a new one
+              if(err) {
+                winston.error('Error in !tip command', err);
+                client.say(channel, settings.messages.error.expand({name: from}));
+                return;
+              }
+
+              coin.send('move', from.toLowerCase(), to.toLowerCase(), amount, function(err, reply) {
+                if(err || !reply) {
+                  winston.error('Error in !tip command', err);
+                  client.say(channel, settings.messages.error.expand({name: from}));
+                  return;
+                }
+
+                winston.info('%s tipped %s %d%s', from, to, amount, settings.coin.short_name)
+                client.say(channel, settings.messages.tipped.expand({from: from, to: to, amount: amount}));
+              });
+            });
+          } else {
+            winston.info('%s tried to tip %s %d, but has only %d', from, to, amount, balance);
+            client.say(channel, settings.messages.no_funds.expand({name: from, balance: balance, short: amount - balance, amount: amount}));
+          }
+        });
+        break;
+        
+      case 'address':
+        var user = from.toLowerCase();
+        client.getAddress(user, function(err, address) {
+          if(err) {
+            winston.error('Error in !address command', err);
+            client.say(channel, settings.messages.error.expand({name: from}));
+            return;
+          }
+
+          client.say(channel, settings.messages.deposit_address.expand({name: user, address: address}));
+        });
+        break;
+        
+		case 'diff':
+        coin.getDifficulty(function(err, get_difficulty) {
+          if(err) {
+            winston.error('Error in !getdiff command', err);
+            client.say(channel, settings.messages.error.expand({name: from}));
+            return;
+          }
+ 		var get_difficulty = typeof(get_difficulty) == 'object' ? get_difficulty.result : get_difficulty;
+
+        client.say(channel, settings.messages.getdiff.expand({diff: get_difficulty}));
+        });
+        break;
+        
+        case 'networkhps':
+        coin.getnetworkhashps(function(err, get_networkhps) {
+          if(err) {
+            winston.error('Error in !networkhps command', err);
+            client.say(channel, settings.messages.error.expand({name: from}));
+            return;
+          }
+ 		var get_networkhps = typeof(get_networkhps) == 'object' ? get_networkhps.result : get_networkhps;
+
+        client.say(channel, settings.messages.networkhps.expand({networkhps: get_networkhps}));
+        });
+        break;
+        
+        
+        
+        
+        
+      case 'balance':
+        var user = from.toLowerCase();
+        coin.getBalance(user, settings.coin.min_confirmations, function(err, balance) {
+          if(err) {
+            winston.error('Error in !balance command', err);
+            client.say(channel, settings.messages.error.expand({name: from}));
+            return;
+          }
+
+          var balance = typeof(balance) == 'object' ? balance.result : balance;
+
+          coin.getBalance(user, 0, function(err, unconfirmed_balance) {
+          if(err) {
+              winston.error('Error in !balance command', err);
+              client.say(channel, settings.messages.balance.expand({balance: balance, name: user}));
+              return;
+            }
+
+            var unconfirmed_balance = typeof(unconfirmed_balance) == 'object' ? unconfirmed_balance.result : unconfirmed_balance;
+
+            client.say(channel, settings.messages.balance_unconfirmed.expand({unconfirmed: unconfirmed_balance}));
+          })
+        });
+        break;
+
+      case 'withdraw':
+        var match = message.match(/^.?withdraw (\S+)$/);
+        if(match == null) {
+          client.say(channel, 'Usage: !withdraw <' + settings.coin.full_name + ' address>');
+          return;
+        }
+        var address = match[1];
+
+        coin.validateAddress(address, function(err, reply) {
+          if(err) {
+            winston.error('Error in !withdraw command', err);
+            client.say(channel, settings.messages.error.expand({name: from}));
+            return;
+          }
+
+          if(reply.isvalid) {
+            coin.getBalance(from.toLowerCase(), settings.coin.min_confirmations, function(err, balance) {
+              if(err) {
+                winston.error('Error in !withdraw command', err);
+                client.say(channel, settings.messages.error.expand({name: from}));
+                return;
+              }
+              var balance = typeof(balance) == 'object' ? balance.result : balance;
+
+              if(balance < settings.coin.min_withdraw) {
+                winston.warn('%s tried to withdraw %d, but min is set to %d', from, balance, settings.coin.min_withdraw);
+                client.say(channel, settings.messages.withdraw_too_small.expand({name: from, balance: balance}));
+                return;
+              }
+
+              coin.sendFrom(from.toLowerCase(), address, balance - settings.coin.withdrawal_fee, function(err, reply) {
+                if(err) {
+                  winston.error('Error in !withdraw command', err);
+                  client.say(channel, settings.messages.error.expand({name: from}));
+                  return;
+                }
+
+                var values = {name: from, address: address, balance: balance, amount: balance - settings.coin.withdrawal_fee, transaction: reply}
+                for(var i = 0; i < settings.messages.withdraw_success.length; i++) {
+                  var msg = settings.messages.withdraw_success[i];
+                  client.say(channel, msg.expand(values));
+                };
+
+                // transfer the rest (usually withdrawal fee - txfee) to bots wallet
+                coin.getBalance(from.toLowerCase(), function(err, balance) {
+                  if(err) {
+                    winston.error('Something went wrong while transferring fees', err);
+                    return;
+                  }
+
+                  var balance = typeof(balance) == 'object' ? balance.result : balance;
+
+                  // moves the rest to bot's wallet
+                  coin.move(from.toLowerCase(), settings.login.nickname.toLowerCase(), balance);
+                });
+              });
+            });
+          } else {
+            winston.warn('%s tried to withdraw to an invalid address', from);
+            client.say(channel, settings.messages.invalid_address.expand({address: address, name: from}));
+          }
+        });
+        break;
+    }
+  });
+});
